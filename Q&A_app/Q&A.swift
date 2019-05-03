3b6b9cd79bd2ffc51114e0602e0d64b3e3a1edb6
Wangsc-app <wangsc0508@gmial.com>
2019-05-03 20:32:13 +0800

    Q&A

---
+++ Q&A_app/Q&A.swift
@@ -0,0 +1,10 @@
+//
+//  Q&A.swift
+//  Q&A_app
+//
+//  Created by wangsc on 2019/4/23.
+//  Copyright © 2019 wangsc. All rights reserved.
+//
+
+import Foundation
+
---
+++ Q&A_app/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Q&A_app
+//
+//  Created by wangsc on 2019/4/23.
+//  Copyright © 2019 wangsc. All rights reserved.
+//
+
+import Foundation
+
+
+class Question {
+    var description = ""
+    var  answer = ""
+}
